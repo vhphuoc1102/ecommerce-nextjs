f549392ea83f726ab2e99da23ba546a24b8ffffd
KAOPIZ\phuocvh <phuocvh@kaopiz.com>
2025-02-04 17:20:58 +0700

    feat: Modify form

--- src/components/product-info-form.tsx
+++ src/components/product-info-form.tsx
@@ -9,60 +9,58 @@ import {ProductAttribute, ProductSku} from "@/libs/types/productType";
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
 import _ from 'lodash'
+import {cn} from "@/libs/utils";
 
 export default function ProductInfoForm({attributes, skus} : {attributes: ProductAttribute[], skus: ProductSku[]}) {
-  const generateDefaultValues = () => {
-    const defaultValues: Record<string, string> = {};
-    return defaultValues;
+  let isDisableAll = false;
+  let isDisableSubmit = false;
+  const defaultValues = skus.filter(sku => sku.stock && sku.stock > 0)[0];
+  if (!defaultValues) {
+      isDisableAll = true;
   }
-  const form = useForm({
-    defaultValues: generateDefaultValues(),
-  })
-  const formAction = () => {};
+  const handleValueChange = (attributeId, value) => {
+
+  }
+  const formAction = (formData: FormData) => {};
   return (
-      <>
-        <Form {...form}>
+      <div>
           <form action={formAction}>
             {
               attributes.map((attribute) => (
-                  <FormField
-                      key={attribute.attributeId}
-                      name={attribute.attributeName}
-                      render={
-                        ({field}) => (
-                            <FormItem className="mb-2">
-                              <FormLabel>{attribute.attributeName}</FormLabel>
-                              <RadioGroup
-                                  onValueChange={field.onChange}
-                                  defaultValue={field.value}
-                                  className="flex gap-2 flex-wrap"
-                              >
-                                {
-                                  attribute.attributeValues.map((value,index) => (
-                                      <FormLabel key={index} className="[&:has([data-state=checked])>div]:border-primary">
-                                        <FormControl>
-                                          <RadioGroupItem value={value} className="sr-only"/>
-                                        </FormControl>
-                                        <Button variant={"outline"}>
-                                          {value}
-                                        </Button>
-                                      </FormLabel>
-                                  ))
-                                }
-
-                              </RadioGroup>
-                            </FormItem>
-                        )
-                      } />
+                  <div key={attribute.attributeId}>
+                    <h3>{attribute.attributeName}</h3>
+                    <RadioGroup
+                        name={attribute.attributeName}
+                        onValueChange={(value) => handleValueChange(attribute.attributeId, value)}
+                        className="flex gap-2 flex-wrap">
+                      {
+                          attribute.attributeValues.map((value,index) => (
+                              <div key={index} className="flex items-center">
+                                  <RadioGroupItem
+                                      value={value}
+                                      id={`${attribute.attributeId}-${index}`}
+                                      className="sr-only peer"
+                                  />
+                                  <Button
+                                      asChild
+                                      variant={"outline"}
+                                      className={cn(
+                                          "peer-data-[state=checked]:bg-primary peer-data-[state=checked]:text-primary-foreground",
+                                          "peer-data-[state=checked]:border-primary",
+                                          "transition-colors",
+                                      )}
+                                  >
+                                      <label htmlFor={`${attribute.attributeId}-${index}`}>{value}</label>
+                                  </Button>
+                              </div>
+                          ))
+                      }
+                    </RadioGroup>
+                  </div>
               ))
             }
           </form>
-        </Form>
-        {/*<div className="flex items-end gap-2 justify-end">*/}
-        {/*  <span className="font-extrabold text-[hsl(var(--destructive))] text-3xl">{productInfo.promotePrice}đ</span>*/}
-        {/*  <span className="text-xl font-semibold text-gray-400 line-through">{productInfo.price}đ</span>*/}
-        {/*</div>*/}
-      </>
+      </div>
   );
 }
 
